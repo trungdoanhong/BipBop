cb314a0a3864a6d9e35d199baee1fb120b9a41e6
trungdoanhong <trungdoanhong@gmail.com>
2019-10-12 22:43:55 +0700

    BipBopCaller

--- BipBopCaller/app/src/main/java/com/example/bipbpcaller/Main2Activity.java
+++ BipBopCaller/app/src/main/java/com/example/bipbpcaller/Main2Activity.java
@@ -2,7 +2,17 @@ package com.example.bipbpcaller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +20,30 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void CallBipBop(View v) {
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://deltaxrobot.com/signal.txt";
+
+        Log.i("tag", url);
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.i("tag", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("tag", "error");
+            }
+        });
+        queue.add(stringRequest);
     }
 }
--- BipBopCaller/app/src/main/java/com/example/bipbpcaller/MainActivity.java
+++ BipBopCaller/app/src/main/java/com/example/bipbpcaller/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.bipbpcaller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etStuffType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitVariables();
     }
+
+    private void InitVariables() {
+        etStuffType =(EditText) findViewById(R.id.etStuffType);
+    }
+
+    public void selectMaterialType(View v)
+    {
+        Button bt = (Button)v;
+
+        Drawable img = (Drawable)bt.getBackground();
+        Drawable imgON = v.getResources().getDrawable(R.drawable.bt2);
+
+        if (img.getConstantState() == imgON.getConstantState())
+        {
+            bt.setBackgroundResource(R.drawable.bt);
+            String currentDes = etStuffType.getText().toString();
+            currentDes = currentDes.replace(", " + bt.getText().toString(), "");
+            etStuffType.setText(currentDes);
+        }
+        else
+        {
+            bt.setBackgroundResource(R.drawable.bt2);
+            String currentDes = etStuffType.getText().toString();
+            currentDes += ", " + bt.getText().toString();
+            etStuffType.setText(currentDes);
+        }
+    }
+
+    public void nextPage(View v) {
+
+        Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
 }
